df87c37c1bf9a2d7ddb62c91fc1e08c866bc3163
fbarretoz <fbarretozerpa@byupathway.edu>
2026-05-06 20:04:06 -0400

    copy right corrected

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,10 @@
+//Get the year
+const today = new Date();
+let year = today.getFullYear();
+
+//Output year
+document.getElementById("currentyear").innerHTML = year;
+
 document.getElementById("lastModified").innerHTML = document.lastModified;
 
 
