75324206a9c12efca3ffa45589ac664a1a56b5a9
fbarretoz <fbarretozerpa@byupathway.edu>
2026-05-30 15:44:50 -0400

    filtered temples final

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -107,14 +107,14 @@ const temples = [
         location: "Aba, Nigeria",
         dedicated: "2005, August, 7",
         area: 11500,
-        imageUrl: "images/temples/caracas_venezuela.webp"
+        imageUrl: "images/temples/aba_nigeria.webp"
     },
     {
         templeName: "Manti Utah",
         location: "Manti, Utah, United States",
         dedicated: "1888, May, 21",
         area: 74792,
-        imageUrl: "images/temples/caracas_venezuela.webp"
+        imageUrl: "images/temples/manti_utah.webp"
     },
     {
         templeName: "Payson Utah",
@@ -128,28 +128,28 @@ const temples = [
         location: "Yigo, Guam",
         dedicated: "2020, May, 2",
         area: 6861,
-        imageUrl: "images/temples/caracas_venezuela.webp"
+        imageUrl: "images/temples/yigo_guam.webp"
     },
     {
         templeName: "Washington D.C.",
         location: "Kensington, Maryland, United States",
         dedicated: "1974, November, 19",
         area: 156558,
-        imageUrl: "images/temples/caracas_venezuela.webp"
+        imageUrl: "images/temples/washington_dc.webp"
     },
     {
         templeName: "Lima Perú",
         location: "Lima, Perú",
         dedicated: "1986, January, 10",
         area: 9600,
-        imageUrl: "images/temples/caracas_venezuela.webp"
+        imageUrl: "images/temples/lima_peru.webp"
     },
     {
         templeName: "Mexico City Mexico",
         location: "Mexico City, Mexico",
         dedicated: "1983, December, 2",
         area: 116642,
-        imageUrl: "images/temples/caracas_venezuela.webp"
+        imageUrl: "images/temples/mexico_city.webp"
     },
     // Add more temple objects here...
     {
