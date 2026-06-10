7406b9c1b6f37927e53de6052f5238d65c89e0c1
fbarretoz <fbarretozerpa@byupathway.edu>
2026-06-10 17:17:30 -0400

    form ultimo

--- scripts/form/review.js
+++ scripts/form/review.js
@@ -18,7 +18,8 @@ let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
 if (numVisits !== 0) {
     visitsDisplay.textContent = numVisits;
 } else {
-    visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+    visitsDisplay.textContent = 1;
+    numVisits++;
 }
 
 // 4️⃣ increment the number of visits by one.
